84cad44192ef306fe288094a8f1ba9fd86c8b0d9
JuriSaki <phamminhdung739@gmail.com>
2026-04-07 22:34:42 +0700

    Add Seller class for managing auction items

---
+++ BTLon/src/Seller.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Seller extends Users {
+    private List<String> itemsForSale = new ArrayList<>();
+
+    // Sử dụng constructor mặc định và gán giá trị thủ công
+    public Seller() {
+        super();
+    }
+    public void addAuctionItem(String itemName) {
+        itemsForSale.add(itemName);
+        System.out.println("Người bán " + this.name + " đã thêm sản phẩm: " + itemName);
+    }
+}
